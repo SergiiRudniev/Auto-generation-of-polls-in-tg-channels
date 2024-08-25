364f7ad881255df2449b13f8795a753d52525173
Sergii Rudniev <100538048+SergiiRudniev@users.noreply.github.com>
2024-08-25 23:45:20 +0200

    Delete tgbot directory

--- tgbot/app/Parser/ParserQuiz.py
+++
@@ -1,8 +0,0 @@
-import logging
-from typing import Tuple, List
-
-
-def Parse(text: str) -> tuple[str, list[str], int]:
-    logging.info(f"Parse Data: {text}")
-    split = text.split("|")
-    return str(split[0]), list(split[1].split(",")), int(split[2])
