985ef89fc0606d8f64e118d7fe10f02f41b98480
Sergii Rudniev <100538048+SergiiRudniev@users.noreply.github.com>
2024-08-28 22:41:44 +0200

    Add files via upload

--- QuizGenerating/app/AI/config.py
+++ QuizGenerating/app/AI/config.py
@@ -1,9 +1,5 @@
-prompt = """Создай опрос по Backend FastApi Flask Django Pydantica Pandas Matplotlib SciPy Scikit-learn TensorFlow Keras Requests BeautifulSoup SQLAlchemyNumpy Aiogram Discord.py и тд на питоне ТОЛЬКО ПО Python, с интересным вопросом уровня middle/senior. По строгой форме ЗАПРОС ДОЛЖЕН БЫТЬ СТРОГО ПО ФОРМЕ И НЕ ИМЕТЬ НИЧЕГО ДРУГОГО
+import logging
+from os import getenv
 
-Форма:
-question|options(list)|correct_option_index
-Пример:
-Пайтон это компелированый или интерпертируемый язык|компелированый,интерпертируемый|1
-ОПРОСЫ НЕ ДОЛЖНЫ ПОВТОРЯТЬСЯ, ОНИ ДОЛЖНЫ БЫТЬ УНИКАЛЬНЫМИ!!!
-ОПРОС ДОЛЖЕН БЫТЬ ТОЛЬКО ОДИН!!! НЕ ДЕЛАЙ МНОГО ОПРОСОВ ЗА РАЗ!!!
-СДЕЛАЙ ВОПРОСЫ УВЛЕКАТЕЛЬНЫМИ, ЧТОБЫ НА НИХ МАЛО КТО ЗНАЛ ОТВЕТ!!!!"""
+prompt = getenv("PROMPT")
+
